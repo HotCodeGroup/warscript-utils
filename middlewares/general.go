cc25fb3688658d39a391602ace5b02a0280328a5
GDVFox <gaaveer@gmail.com>
2019-04-26 23:54:25 +0300

    [UPD] WithAuthentiфиcation uses gPRC

--- middlewares/general.go
+++ middlewares/general.go
@@ -5,11 +5,44 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/HotCodeGroup/warscript-utils/models"
 	"github.com/HotCodeGroup/warscript-utils/utils"
+	"github.com/pkg/errors"
 	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/time/rate"
 )
+
+const (
+	// SessionInfoKey ключ, по которому в контексте
+	// реквеста хранится структура юзера после валидации
+	SessionInfoKey utils.ContextKey = 1
+)
+
+// WithAuthentication проверка токена перед исполнением запроса
+//nolint: interfacer
+func WithAuthentication(next http.HandlerFunc, l *logrus.Logger, cli models.AuthClient) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		logger := utils.GetLogger(r, l, "CheckUsername")
+		errWriter := utils.NewErrorResponseWriter(w, logger)
+
+		cookie, err := r.Cookie("JSESSIONID")
+		if err != nil || cookie == nil {
+			errWriter.WriteWarn(http.StatusUnauthorized, errors.Wrap(err, "can not load cookie"))
+			return
+		}
+
+		session, err := cli.GetSessionInfo(r.Context(), &models.SessionToken{Token: cookie.Value})
+		if err != nil {
+			errWriter.WriteError(http.StatusUnauthorized, errors.Wrap(err, "get session error"))
+			return
+
+		}
+
+		ctx := context.WithValue(r.Context(), SessionInfoKey, session)
+		next.ServeHTTP(w, r.WithContext(ctx))
+	})
+}
 
 // RecoverMiddleware ловит паники и кидает 500ки
 func RecoverMiddleware(next http.Handler, log *logrus.Logger) http.Handler {
