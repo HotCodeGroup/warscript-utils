b42aea32f8046b4efafd8a46f446d34085f8e7d3
GDVFox <gaaveer@gmail.com>
2019-04-26 22:42:45 +0300

    [ADD] WithLimiter method

--- go.mod
+++ go.mod
@@ -6,4 +6,5 @@ require (
 	github.com/pkg/errors v0.8.1
 	github.com/satori/go.uuid v1.2.0
 	github.com/sirupsen/logrus v1.4.1
+	golang.org/x/time v0.0.0-20190308202827-9d24e82272b4
 )
--- middlewares/general.go
+++ middlewares/general.go
@@ -8,6 +8,7 @@ import (
 	"github.com/HotCodeGroup/warscript-utils/utils"
 	uuid "github.com/satori/go.uuid"
 	"github.com/sirupsen/logrus"
+	"golang.org/x/time/rate"
 )
 
 // RecoverMiddleware ловит паники и кидает 500ки
@@ -38,5 +39,19 @@ func AccessLogMiddleware(next http.Handler, log *logrus.Logger) http.Handler {
 			"method":    r.Method,
 			"work_time": time.Since(start).Seconds(),
 		}).Info(r.URL.Path)
+	})
+}
+
+// WithLimiter для запросов, у которых есть ограничение в секунду
+//nolint: interfacer
+func WithLimiter(next http.HandlerFunc, limiter *rate.Limiter, log *logrus.Logger) http.HandlerFunc {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if !limiter.Allow() {
+			log.WithField("method", "WithLimiter").Warn("too many requests")
+			http.Error(w, "", http.StatusTooManyRequests)
+			return
+		}
+
+		next.ServeHTTP(w, r)
 	})
 }
