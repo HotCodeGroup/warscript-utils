f8235b4318574db8707c6b24c939efbf3ad04ccc
GDVFox <gaaveer@gmail.com>
2019-05-03 19:04:51 +0300

    [UPD] Auth models

--- models/auth.pb.go
+++ models/auth.pb.go
@@ -34,7 +34,7 @@ func (m *UserID) Reset()         { *m = UserID{} }
 func (m *UserID) String() string { return proto.CompactTextString(m) }
 func (*UserID) ProtoMessage()    {}
 func (*UserID) Descriptor() ([]byte, []int) {
-	return fileDescriptor_auth_42a0a9061e19377b, []int{0}
+	return fileDescriptor_auth_c2e782b57a3be520, []int{0}
 }
 func (m *UserID) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_UserID.Unmarshal(m, b)
@@ -75,7 +75,7 @@ func (m *InfoUser) Reset()         { *m = InfoUser{} }
 func (m *InfoUser) String() string { return proto.CompactTextString(m) }
 func (*InfoUser) ProtoMessage()    {}
 func (*InfoUser) Descriptor() ([]byte, []int) {
-	return fileDescriptor_auth_42a0a9061e19377b, []int{1}
+	return fileDescriptor_auth_c2e782b57a3be520, []int{1}
 }
 func (m *InfoUser) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_InfoUser.Unmarshal(m, b)
@@ -134,7 +134,7 @@ func (m *SessionToken) Reset()         { *m = SessionToken{} }
 func (m *SessionToken) String() string { return proto.CompactTextString(m) }
 func (*SessionToken) ProtoMessage()    {}
 func (*SessionToken) Descriptor() ([]byte, []int) {
-	return fileDescriptor_auth_42a0a9061e19377b, []int{2}
+	return fileDescriptor_auth_c2e782b57a3be520, []int{2}
 }
 func (m *SessionToken) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SessionToken.Unmarshal(m, b)
@@ -172,7 +172,7 @@ func (m *SessionPayload) Reset()         { *m = SessionPayload{} }
 func (m *SessionPayload) String() string { return proto.CompactTextString(m) }
 func (*SessionPayload) ProtoMessage()    {}
 func (*SessionPayload) Descriptor() ([]byte, []int) {
-	return fileDescriptor_auth_42a0a9061e19377b, []int{3}
+	return fileDescriptor_auth_c2e782b57a3be520, []int{3}
 }
 func (m *SessionPayload) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_SessionPayload.Unmarshal(m, b)
@@ -199,11 +199,89 @@ func (m *SessionPayload) GetID() int64 {
 	return 0
 }
 
+type UserIDs struct {
+	IDs                  []*UserID `protobuf:"bytes,1,rep,name=IDs,proto3" json:"IDs,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}  `json:"-"`
+	XXX_unrecognized     []byte    `json:"-"`
+	XXX_sizecache        int32     `json:"-"`
+}
+
+func (m *UserIDs) Reset()         { *m = UserIDs{} }
+func (m *UserIDs) String() string { return proto.CompactTextString(m) }
+func (*UserIDs) ProtoMessage()    {}
+func (*UserIDs) Descriptor() ([]byte, []int) {
+	return fileDescriptor_auth_c2e782b57a3be520, []int{4}
+}
+func (m *UserIDs) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_UserIDs.Unmarshal(m, b)
+}
+func (m *UserIDs) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_UserIDs.Marshal(b, m, deterministic)
+}
+func (dst *UserIDs) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_UserIDs.Merge(dst, src)
+}
+func (m *UserIDs) XXX_Size() int {
+	return xxx_messageInfo_UserIDs.Size(m)
+}
+func (m *UserIDs) XXX_DiscardUnknown() {
+	xxx_messageInfo_UserIDs.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_UserIDs proto.InternalMessageInfo
+
+func (m *UserIDs) GetIDs() []*UserID {
+	if m != nil {
+		return m.IDs
+	}
+	return nil
+}
+
+type InfoUsers struct {
+	Users                []*InfoUser `protobuf:"bytes,1,rep,name=users,proto3" json:"users,omitempty"`
+	XXX_NoUnkeyedLiteral struct{}    `json:"-"`
+	XXX_unrecognized     []byte      `json:"-"`
+	XXX_sizecache        int32       `json:"-"`
+}
+
+func (m *InfoUsers) Reset()         { *m = InfoUsers{} }
+func (m *InfoUsers) String() string { return proto.CompactTextString(m) }
+func (*InfoUsers) ProtoMessage()    {}
+func (*InfoUsers) Descriptor() ([]byte, []int) {
+	return fileDescriptor_auth_c2e782b57a3be520, []int{5}
+}
+func (m *InfoUsers) XXX_Unmarshal(b []byte) error {
+	return xxx_messageInfo_InfoUsers.Unmarshal(m, b)
+}
+func (m *InfoUsers) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	return xxx_messageInfo_InfoUsers.Marshal(b, m, deterministic)
+}
+func (dst *InfoUsers) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_InfoUsers.Merge(dst, src)
+}
+func (m *InfoUsers) XXX_Size() int {
+	return xxx_messageInfo_InfoUsers.Size(m)
+}
+func (m *InfoUsers) XXX_DiscardUnknown() {
+	xxx_messageInfo_InfoUsers.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_InfoUsers proto.InternalMessageInfo
+
+func (m *InfoUsers) GetUsers() []*InfoUser {
+	if m != nil {
+		return m.Users
+	}
+	return nil
+}
+
 func init() {
 	proto.RegisterType((*UserID)(nil), "models.UserID")
 	proto.RegisterType((*InfoUser)(nil), "models.InfoUser")
 	proto.RegisterType((*SessionToken)(nil), "models.SessionToken")
 	proto.RegisterType((*SessionPayload)(nil), "models.SessionPayload")
+	proto.RegisterType((*UserIDs)(nil), "models.UserIDs")
+	proto.RegisterType((*InfoUsers)(nil), "models.InfoUsers")
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -220,6 +298,7 @@ const _ = grpc.SupportPackageIsVersion4
 type AuthClient interface {
 	GetUserByID(ctx context.Context, in *UserID, opts ...grpc.CallOption) (*InfoUser, error)
 	GetSessionInfo(ctx context.Context, in *SessionToken, opts ...grpc.CallOption) (*SessionPayload, error)
+	GetUsersByIDs(ctx context.Context, in *UserIDs, opts ...grpc.CallOption) (*InfoUsers, error)
 }
 
 type authClient struct {
@@ -248,10 +327,20 @@ func (c *authClient) GetSessionInfo(ctx context.Context, in *SessionToken, opts 
 	return out, nil
 }
 
+func (c *authClient) GetUsersByIDs(ctx context.Context, in *UserIDs, opts ...grpc.CallOption) (*InfoUsers, error) {
+	out := new(InfoUsers)
+	err := c.cc.Invoke(ctx, "/models.Auth/GetUsersByIDs", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // AuthServer is the server API for Auth service.
 type AuthServer interface {
 	GetUserByID(context.Context, *UserID) (*InfoUser, error)
 	GetSessionInfo(context.Context, *SessionToken) (*SessionPayload, error)
+	GetUsersByIDs(context.Context, *UserIDs) (*InfoUsers, error)
 }
 
 func RegisterAuthServer(s *grpc.Server, srv AuthServer) {
@@ -294,6 +383,24 @@ func _Auth_GetSessionInfo_Handler(srv interface{}, ctx context.Context, dec func
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Auth_GetUsersByIDs_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UserIDs)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(AuthServer).GetUsersByIDs(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/models.Auth/GetUsersByIDs",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(AuthServer).GetUsersByIDs(ctx, req.(*UserIDs))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Auth_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "models.Auth",
 	HandlerType: (*AuthServer)(nil),
@@ -306,28 +413,36 @@ var _Auth_serviceDesc = grpc.ServiceDesc{
 			MethodName: "GetSessionInfo",
 			Handler:    _Auth_GetSessionInfo_Handler,
 		},
+		{
+			MethodName: "GetUsersByIDs",
+			Handler:    _Auth_GetUsersByIDs_Handler,
+		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "auth.proto",
 }
 
-func init() { proto.RegisterFile("auth.proto", fileDescriptor_auth_42a0a9061e19377b) }
+func init() { proto.RegisterFile("auth.proto", fileDescriptor_auth_c2e782b57a3be520) }
 
-var fileDescriptor_auth_42a0a9061e19377b = []byte{
-	// 238 bytes of a gzipped FileDescriptorProto
-	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0x4a, 0x2c, 0x2d, 0xc9,
-	0xd0, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xcb, 0xcd, 0x4f, 0x49, 0xcd, 0x29, 0x56, 0x92,
-	0xe0, 0x62, 0x0b, 0x2d, 0x4e, 0x2d, 0xf2, 0x74, 0x11, 0xe2, 0xe3, 0x62, 0xf2, 0x74, 0x91, 0x60,
-	0x54, 0x60, 0xd4, 0x60, 0x0e, 0x02, 0xb2, 0x94, 0x72, 0xb8, 0x38, 0x3c, 0xf3, 0xd2, 0xf2, 0x41,
-	0xb2, 0xe8, 0x72, 0x42, 0x52, 0x5c, 0x1c, 0xa5, 0x40, 0xf1, 0xbc, 0xc4, 0xdc, 0x54, 0x09, 0x26,
-	0xa0, 0x28, 0x67, 0x10, 0x9c, 0x2f, 0x24, 0xc3, 0xc5, 0x59, 0x90, 0x01, 0xb4, 0x22, 0x34, 0x14,
-	0xa8, 0x85, 0x19, 0x2c, 0x89, 0x10, 0x10, 0x12, 0xe3, 0x62, 0x4b, 0x4c, 0x2e, 0xc9, 0x2c, 0x4b,
-	0x95, 0x60, 0x01, 0x4a, 0x71, 0x04, 0x41, 0x79, 0x4a, 0x2a, 0x5c, 0x3c, 0xc1, 0xa9, 0xc5, 0xc5,
-	0x99, 0xf9, 0x79, 0x21, 0xf9, 0xd9, 0xa9, 0x79, 0x42, 0x22, 0x5c, 0xac, 0x25, 0x20, 0x06, 0xd8,
-	0x52, 0xce, 0x20, 0x08, 0x47, 0x49, 0x81, 0x8b, 0x0f, 0xaa, 0x2a, 0x20, 0xb1, 0x32, 0x27, 0x3f,
-	0x31, 0x05, 0xdd, 0x65, 0x46, 0xe5, 0x5c, 0x2c, 0x8e, 0x40, 0x5f, 0x0a, 0xe9, 0x73, 0x71, 0xbb,
-	0xa7, 0x96, 0x80, 0x1c, 0xef, 0x54, 0x09, 0xf2, 0x9c, 0x1e, 0xc4, 0xbf, 0x7a, 0x10, 0xcf, 0x4a,
-	0x09, 0xc0, 0xf8, 0x70, 0x2f, 0xda, 0x71, 0xf1, 0x01, 0x35, 0x40, 0x4d, 0x07, 0x89, 0x0a, 0x89,
-	0xc0, 0xd4, 0x20, 0x3b, 0x4c, 0x4a, 0x0c, 0x4d, 0x14, 0xea, 0x90, 0x24, 0x36, 0x70, 0xb8, 0x1a,
-	0x03, 0x02, 0x00, 0x00, 0xff, 0xff, 0xe5, 0xa7, 0x0e, 0x4d, 0x65, 0x01, 0x00, 0x00,
+var fileDescriptor_auth_c2e782b57a3be520 = []byte{
+	// 294 bytes of a gzipped FileDescriptorProto
+	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0x64, 0x91, 0xd1, 0x4b, 0x83, 0x50,
+	0x14, 0xc6, 0xd9, 0xdc, 0x4c, 0xcf, 0xca, 0xea, 0x30, 0x86, 0x48, 0x0f, 0x22, 0x11, 0x83, 0xc0,
+	0x60, 0xbe, 0x07, 0xc5, 0x20, 0x7c, 0x8b, 0x5b, 0xfe, 0x01, 0xd6, 0x6e, 0x6c, 0xe4, 0xbc, 0x63,
+	0xc7, 0x05, 0xfb, 0x9f, 0xfa, 0x23, 0x3b, 0x57, 0xbd, 0xd5, 0xdc, 0x93, 0xf7, 0x7c, 0xbf, 0xf3,
+	0xf9, 0xf9, 0x5d, 0x01, 0xf2, 0x5d, 0xb5, 0x8c, 0x37, 0x5b, 0x55, 0x29, 0xb4, 0xd7, 0x6a, 0x21,
+	0x0b, 0x8a, 0x7c, 0xb0, 0x33, 0x92, 0xdb, 0x74, 0x8e, 0x1e, 0xf4, 0xd3, 0xb9, 0xdf, 0x0b, 0x7b,
+	0x53, 0x4b, 0xf0, 0x29, 0x2a, 0xc0, 0x49, 0xcb, 0x0f, 0xa5, 0x69, 0x97, 0x61, 0x00, 0xce, 0x8e,
+	0xf5, 0x32, 0x5f, 0x4b, 0xbf, 0xcf, 0xaa, 0x2b, 0x7e, 0x67, 0xbc, 0x02, 0x77, 0xb3, 0xe4, 0x88,
+	0x2c, 0x63, 0x8b, 0x55, 0xc3, 0x3f, 0x01, 0x27, 0x60, 0xe7, 0xef, 0xd5, 0xea, 0x4b, 0xfa, 0x03,
+	0x46, 0x8e, 0x68, 0xa7, 0xe8, 0x1a, 0x4e, 0x5f, 0x24, 0xd1, 0x4a, 0x95, 0xaf, 0xea, 0x53, 0x96,
+	0x38, 0x86, 0x61, 0xa5, 0x0f, 0x75, 0xa8, 0x2b, 0x9a, 0x21, 0x0a, 0xc1, 0x6b, 0xb7, 0x9e, 0xf3,
+	0x7d, 0xa1, 0xf2, 0xc5, 0xd1, 0x57, 0xdf, 0xc2, 0x49, 0xd3, 0x87, 0x30, 0x04, 0x8b, 0x1f, 0xcc,
+	0xac, 0xe9, 0x68, 0xe6, 0xc5, 0x4d, 0xe1, 0xb8, 0xa1, 0x42, 0xa3, 0x28, 0x01, 0xd7, 0x54, 0x24,
+	0xbc, 0x81, 0xa1, 0xee, 0x60, 0x0c, 0x17, 0xc6, 0x60, 0x36, 0x44, 0x83, 0x67, 0xdf, 0x3d, 0x18,
+	0x3c, 0xf0, 0x45, 0xe2, 0x1d, 0x8c, 0x9e, 0x64, 0xa5, 0xd1, 0xe3, 0x5e, 0xdf, 0xdf, 0x61, 0x42,
+	0x70, 0xf4, 0x02, 0xbc, 0x07, 0x8f, 0x0d, 0x6d, 0x01, 0xad, 0xe2, 0xd8, 0xec, 0xfc, 0xef, 0x1e,
+	0x4c, 0x3a, 0xaa, 0xe9, 0x9a, 0xc0, 0x59, 0x1b, 0x48, 0x3a, 0x91, 0xf0, 0xfc, 0x30, 0x92, 0x82,
+	0xcb, 0x6e, 0x26, 0xbd, 0xd9, 0xf5, 0xff, 0x4e, 0x7e, 0x02, 0x00, 0x00, 0xff, 0xff, 0x14, 0x3b,
+	0xc3, 0xb3, 0xfd, 0x01, 0x00, 0x00,
 }
