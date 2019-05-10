3daf74b9585cf4a6699edc33b99a78d4e2b6718b
GDVFox <gaaveer@gmail.com>
2019-05-10 22:25:03 +0300

    [UPD] PQ in use

--- postgresql/postgresql.go
+++ postgresql/postgresql.go
@@ -1,35 +1,52 @@
 package postgresql
 
 import (
-	"github.com/pkg/errors"
+	"database/sql"
+	"fmt"
 	"strconv"
 
-	"github.com/jackc/pgx"
+	"github.com/pkg/errors"
 )
 
 type Queryer interface {
-	QueryRow(string, ...interface{}) *pgx.Row
+	QueryRow(string, ...interface{}) *sql.Row
 }
 
-func Connect(dbUser, dbPass, dbHost, dbPort, dbName string) (*pgx.ConnPool, error) {
+func Connect(dbUser, dbPass, dbHost, dbPort, dbName string) (*sql.DB, error) {
 	var err error
-	port, err := strconv.ParseInt(dbPort, 10, 16)
+	_, err = strconv.ParseInt(dbPort, 10, 16)
 	if err != nil {
 		return nil, errors.Wrap(err, "port int parse error")
 	}
 
-	pgxConn, err := pgx.NewConnPool(pgx.ConnPoolConfig{
-		ConnConfig: pgx.ConnConfig{
-			Host:     dbHost,
-			User:     dbUser,
-			Password: dbPass,
-			Database: dbName,
-			Port:     uint16(port),
-		},
-	})
+	pqConn, err := sql.Open("postgres", fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable",
+		dbUser, dbPass, dbHost, dbPort, dbName))
 	if err != nil {
-		return nil, errors.Wrap(err, "can not create pgx connection pool")
+		return nil, errors.Wrap(err, "can not create pq connection")
 	}
 
-	return pgxConn, nil
+	return pqConn, nil
 }
+
+// func Connect(dbUser, dbPass, dbHost, dbPort, dbName string) (*pgx.ConnPool, error) {
+// 	var err error
+// 	port, err := strconv.ParseInt(dbPort, 10, 16)
+// 	if err != nil {
+// 		return nil, errors.Wrap(err, "port int parse error")
+// 	}
+
+// 	pgxConn, err := pgx.NewConnPool(pgx.ConnPoolConfig{
+// 		ConnConfig: pgx.ConnConfig{
+// 			Host:     dbHost,
+// 			User:     dbUser,
+// 			Password: dbPass,
+// 			Database: dbName,
+// 			Port:     uint16(port),
+// 		},
+// 	})
+// 	if err != nil {
+// 		return nil, errors.Wrap(err, "can not create pgx connection pool")
+// 	}
+
+// 	return pgxConn, nil
+// }
