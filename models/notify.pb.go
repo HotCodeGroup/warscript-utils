e73b9b79228f0b21ff9bffaef248efeb25b4b5b1
GDVFox <gaaveer@gmail.com>
2019-05-12 15:48:07 +0300

    [ADD] Notify model v2

--- models/notify.pb.go
+++ models/notify.pb.go
@@ -33,7 +33,7 @@ func (m *Empty) Reset()         { *m = Empty{} }
 func (m *Empty) String() string { return proto.CompactTextString(m) }
 func (*Empty) ProtoMessage()    {}
 func (*Empty) Descriptor() ([]byte, []int) {
-	return fileDescriptor_notify_ebb4b2aaf0ea5886, []int{0}
+	return fileDescriptor_notify_c547f2f2105ce0aa, []int{0}
 }
 func (m *Empty) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Empty.Unmarshal(m, b)
@@ -67,7 +67,7 @@ func (m *Message) Reset()         { *m = Message{} }
 func (m *Message) String() string { return proto.CompactTextString(m) }
 func (*Message) ProtoMessage()    {}
 func (*Message) Descriptor() ([]byte, []int) {
-	return fileDescriptor_notify_ebb4b2aaf0ea5886, []int{1}
+	return fileDescriptor_notify_c547f2f2105ce0aa, []int{1}
 }
 func (m *Message) XXX_Unmarshal(b []byte) error {
 	return xxx_messageInfo_Message.Unmarshal(m, b)
@@ -132,7 +132,7 @@ const _ = grpc.SupportPackageIsVersion4
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type NotifyClient interface {
-	GetUsersByIDs(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Empty, error)
+	SendNotify(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Empty, error)
 }
 
 type notifyClient struct {
@@ -143,9 +143,9 @@ func NewNotifyClient(cc *grpc.ClientConn) NotifyClient {
 	return &notifyClient{cc}
 }
 
-func (c *notifyClient) GetUsersByIDs(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Empty, error) {
+func (c *notifyClient) SendNotify(ctx context.Context, in *Message, opts ...grpc.CallOption) (*Empty, error) {
 	out := new(Empty)
-	err := c.cc.Invoke(ctx, "/models.Notify/GetUsersByIDs", in, out, opts...)
+	err := c.cc.Invoke(ctx, "/models.Notify/SendNotify", in, out, opts...)
 	if err != nil {
 		return nil, err
 	}
@@ -154,27 +154,27 @@ func (c *notifyClient) GetUsersByIDs(ctx context.Context, in *Message, opts ...g
 
 // NotifyServer is the server API for Notify service.
 type NotifyServer interface {
-	GetUsersByIDs(context.Context, *Message) (*Empty, error)
+	SendNotify(context.Context, *Message) (*Empty, error)
 }
 
 func RegisterNotifyServer(s *grpc.Server, srv NotifyServer) {
 	s.RegisterService(&_Notify_serviceDesc, srv)
 }
 
-func _Notify_GetUsersByIDs_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+func _Notify_SendNotify_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
 	in := new(Message)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(NotifyServer).GetUsersByIDs(ctx, in)
+		return srv.(NotifyServer).SendNotify(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: "/models.Notify/GetUsersByIDs",
+		FullMethod: "/models.Notify/SendNotify",
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(NotifyServer).GetUsersByIDs(ctx, req.(*Message))
+		return srv.(NotifyServer).SendNotify(ctx, req.(*Message))
 	}
 	return interceptor(ctx, in, info, handler)
 }
@@ -184,27 +184,26 @@ var _Notify_serviceDesc = grpc.ServiceDesc{
 	HandlerType: (*NotifyServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "GetUsersByIDs",
-			Handler:    _Notify_GetUsersByIDs_Handler,
+			MethodName: "SendNotify",
+			Handler:    _Notify_SendNotify_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
 	Metadata: "notify.proto",
 }
 
-func init() { proto.RegisterFile("notify.proto", fileDescriptor_notify_ebb4b2aaf0ea5886) }
+func init() { proto.RegisterFile("notify.proto", fileDescriptor_notify_c547f2f2105ce0aa) }
 
-var fileDescriptor_notify_ebb4b2aaf0ea5886 = []byte{
-	// 164 bytes of a gzipped FileDescriptorProto
+var fileDescriptor_notify_c547f2f2105ce0aa = []byte{
+	// 157 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x09, 0x6e, 0x88, 0x02, 0xff, 0xe2, 0xe2, 0xc9, 0xcb, 0x2f, 0xc9,
 	0x4c, 0xab, 0xd4, 0x2b, 0x28, 0xca, 0x2f, 0xc9, 0x17, 0x62, 0xcb, 0xcd, 0x4f, 0x49, 0xcd, 0x29,
 	0x56, 0x62, 0xe7, 0x62, 0x75, 0xcd, 0x2d, 0x28, 0xa9, 0x54, 0x8a, 0xe4, 0x62, 0xf7, 0x4d, 0x2d,
 	0x2e, 0x4e, 0x4c, 0x4f, 0x15, 0x12, 0xe2, 0x62, 0x29, 0xa9, 0x2c, 0x48, 0x95, 0x60, 0x54, 0x60,
 	0xd4, 0xe0, 0x0c, 0x02, 0xb3, 0x41, 0x62, 0xa5, 0xc5, 0xa9, 0x45, 0x12, 0x4c, 0x40, 0x31, 0xe6,
 	0x20, 0x30, 0x1b, 0x24, 0x96, 0x9e, 0x98, 0x9b, 0x2a, 0xc1, 0x0c, 0x51, 0x07, 0x62, 0x83, 0xc4,
-	0x92, 0xf2, 0x53, 0x2a, 0x25, 0x58, 0x80, 0x62, 0x3c, 0x41, 0x60, 0xb6, 0x91, 0x25, 0x17, 0x9b,
-	0x1f, 0xd8, 0x6e, 0x21, 0x7d, 0x2e, 0x5e, 0xf7, 0xd4, 0x92, 0x50, 0xa0, 0xe6, 0x62, 0xa7, 0x4a,
-	0x4f, 0x97, 0x62, 0x21, 0x7e, 0x3d, 0x88, 0x3b, 0xf4, 0xa0, 0x76, 0x4b, 0xf1, 0xc2, 0x04, 0xc0,
-	0xae, 0x4a, 0x62, 0x03, 0xbb, 0xd6, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x5b, 0xe8, 0x25, 0x73,
-	0xbd, 0x00, 0x00, 0x00,
+	0x92, 0xf2, 0x53, 0x2a, 0x25, 0x58, 0x80, 0x62, 0x3c, 0x41, 0x60, 0xb6, 0x91, 0x19, 0x17, 0x9b,
+	0x1f, 0xd8, 0x6e, 0x21, 0x1d, 0x2e, 0xae, 0xe0, 0xd4, 0xbc, 0x14, 0x28, 0x8f, 0x5f, 0x0f, 0xe2,
+	0x08, 0x3d, 0xa8, 0xc5, 0x52, 0xbc, 0x30, 0x01, 0xb0, 0x93, 0x92, 0xd8, 0xc0, 0x4e, 0x35, 0x06,
+	0x04, 0x00, 0x00, 0xff, 0xff, 0x51, 0x4e, 0xa7, 0x1b, 0xba, 0x00, 0x00, 0x00,
 }
